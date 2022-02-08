849ecc8d280739bbcc24cc9c301dcf25b7c30a29
Jillian Morgan <jillianjones@clayton.edu>
2022-02-07 22:33:48 -0500

    called the invert and negate methods

--- Classes & Objects/RationalDemo.java
+++ Classes & Objects/RationalDemo.java
@@ -1,15 +1,20 @@
 public class RationalDemo{
     public static void main(String[] args){
-        Rational r1 = new Rational();
-        System.out.println(r1.getNum() + " / " + r1.getDen());
-       //  r1.setNum(12);
-//         r1.setDen(11);
+//         Rational r1 = new Rational();
 //         System.out.println(r1.getNum() + " / " + r1.getDen());
-        
-        Rational r2 = new Rational();
-        System.out.println(r2.getNum() + " / " + r2.getDen());
+//        //  r1.setNum(12);
+// //         r1.setDen(11);
+// //         System.out.println(r1.getNum() + " / " + r1.getDen());
+//         
+//         Rational r2 = new Rational();
+//         System.out.println(r2.getNum() + " / " + r2.getDen());
         
         Rational r3 = new Rational(2, 3);
         System.out.println(r3.getNum() + " / " + r3.getDen());
+        r3.negate();
+        System.out.println(r3.getNum() + " / " + r3.getDen());
+        r3.invert();
+        System.out.println(r3.getNum() + " / " + r3.getDen());
+        
     }
 }
