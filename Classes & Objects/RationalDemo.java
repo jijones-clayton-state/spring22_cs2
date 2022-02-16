a483b526d3bfdcf6900b35fea91ab8bc8ccf44e5
Jillian Morgan <jillianmorgan@clayton.edu>
2022-02-16 17:07:58 -0500

    updated driver for toString and equals

--- Classes & Objects/RationalDemo.java
+++ Classes & Objects/RationalDemo.java
@@ -1,22 +1,9 @@
 public class RationalDemo{
-    public static void main(String[] args){
-//         Rational r1 = new Rational();
-//         System.out.println(r1.getNum() + " / " + r1.getDen());
-//        //  r1.setNum(12);
-// //         r1.setDen(11);
-// //         System.out.println(r1.getNum() + " / " + r1.getDen());
-//         
-//         Rational r2 = new Rational();
-//         System.out.println(r2.getNum() + " / " + r2.getDen());
-        
+    public static void main(String[] args){        
         Rational r3 = new Rational(2, 3);
-  //       System.out.println(r3.getNum() + " / " + r3.getDen());
-//         r3.negate();
-//         System.out.println(r3.getNum() + " / " + r3.getDen());
-//         r3.invert();
-//         System.out.println(r3.getNum() + " / " + r3.getDen());
-        
+                
         System.out.println(r3);
+        
         String stuff = "This number = " + r3;
         System.out.println(stuff);
         
