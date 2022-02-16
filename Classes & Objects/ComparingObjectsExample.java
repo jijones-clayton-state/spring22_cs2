c0887cbee0669e486b0a4b88f071ddc62943a3bd
Jillian Morgan <jillianmorgan@clayton.edu>
2022-02-16 17:08:24 -0500

    example for how comparing objects works

---
+++ Classes & Objects/ComparingObjectsExample.java
@@ -0,0 +1,15 @@
+public class ComparingObjectsExample{
+    public static void main(String[] args){
+        Rational r1 = new Rational(3, 5);
+        Rational r2 = new Rational(3, 5);
+        
+        int x = 5;
+        int y = 5;
+        
+        System.out.println(x == y);
+        
+        
+        System.out.println(r1 == r2);
+        
+    }
+}
